b0a3c8c13809a25e092effeef82346c9a83ced1d
Jorden Ross <JRoss9117@gmail.com>
2024-03-18 23:48:15 -0400

    More changes

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -46,44 +46,49 @@ public class CatTest {
     public void testSetBirthDate(){
 // Given (a name exists and a cat exists)
         Cat cat = new Cat(null, null, null);
-        Date birthDate = new Date(1992);
+        Date birthDate = new Date(1992, 12, 24);
 
         // When (a cat's birthdate is expected)
         cat.setBirthDate(birthDate);
 
         // Then (we expect to get the given birthday from the dog)
-        Date catBirthDate = cat.setBirthDate(Date1992);
-        Assert.assertEquals(catBirthDate, setBirthDate);
+        Date actual = cat.getBirthDate();
+        Assert.assertEquals(birthDate, actual);
     }
     // TODO - Create tests for `void eat(Food food)`
     @Test
     public void testEat(){
         // Given (a name exists and a cat exists)
         Cat cat = new Cat(null, null, null);
-        String givenEat = "Kibbles";
+        Food food = new Food();
 
         // When (a cat to eat is expected)
-        cat.setEat(givenEat);
+        cat.eat(food);
 
-        // Then (we expect to get the given birthday from the dog)
-        String catEat = cat.getEat();
-        Assert.assertEquals(catEat, givenEat);
+        
+        Assert.assertTrue(cat.getNumberOfMealsEaten() > 0);
 
     }
     // TODO - Create tests for `Integer getId()`
     @Test
     public void testGetId(){
-
+Cat cat = new Cat(null, null, 89456);
+    int expected  = 89456;
+        int actual = cat.getId();
+        Assert.assertEquals(expected, actual);
     }
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
     @Test
     public void testCheckAnimalInheritance(){
+        Cat cat = new Cat(null, null, null);
+        Assert.assertTrue(true);
 
     }
     // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
     @Test
     public void testCheckMammalInheritance(){
-
+        Cat cat = new Cat(null,null,null);
+        Assert.assertTrue(true);
     }
 
 
